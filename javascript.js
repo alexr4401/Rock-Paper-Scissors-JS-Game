fb65eb6df887b3f8e2781308708eb88ca3718cd2
Alex Rodriguez <alexr4401@gmail.com>
2022-09-19 23:20:22 -0400

    intial commit

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function getComputerChoice(rps) {
+    return Math.random();
+    if (getComputerChoice = 0.34) {
+        {getComputerChoice = 'Rock';}
+    }
+   
+}
+
+if (getComputerChoice <.034) {
+    {
+        getComputerChoice = 'Rock';
+    }
+
+}
+
+console.log(getComputerChoice());
